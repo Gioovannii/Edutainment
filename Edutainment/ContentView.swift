152ad17252052ad5606e07115b712f12fff1178b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-13 14:25:40 +0800

    add new state properties

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var settingDone = false
     @State private var multiplicationTable = 1
     @State private var questionSelection = 1
+    
     let questions = ["5", "10", "20", "All"]
     
     var body: some View {
