a2bd46cba4071411852fbee946b9ea39a6f7a22d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-13 13:40:56 +0800

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Giovanni Gaffé on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
