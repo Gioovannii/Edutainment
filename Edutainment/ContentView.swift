0b27cc467293f0e45d3e255553d3f6d6e415a997
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-29 14:55:20 +0800

    generate questions and get current selection

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -11,38 +11,75 @@ struct ContentView: View {
     @State private var settingDone = false
     @State private var multiplicationTable = 1
     @State private var questionSelection = 1
+    @State private var questions = [Question]()
     
-    let questions = ["5", "10", "20", "All"]
-    
+    let numberOfQuestions = ["5", "10", "20", "All"]
     var body: some View {
         NavigationView {
-            Form {
-                Group {
-                    Section(header: Text("How many questions ?")) {
-                        Picker("Questions amount", selection: $questionSelection) {
-                            ForEach(0 ..< questions.count) {
-                                Text("\(self.questions[$0])")
+            Group {
+                if settingDone {
+                    GameView(questions: questions, numberQuestion: getNumberQuestion()) {
+                        self.settingDone = false
+                    }
+                } else {
+                    Form {
+                        Section(header: Text("Which multiplication table ?")) {
+                            Stepper(value: $multiplicationTable, in: 1...12) {
+                                Text("Multiplication table \(multiplicationTable)")
+                            }
+                            .animation(.default)
+                        }
+                        
+                        
+                        Section(header: Text("How many questions ?")) {
+                            Picker("Questions amount", selection: $questionSelection) {
+                                ForEach(0 ..< numberOfQuestions.count) {
+                                    Text("\(self.numberOfQuestions[$0])")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                        
+                        Section {
+                            Button("Start game") {
+                                settingDone.toggle()
+                                self.generateQuestion()
                             }
                         }
-                        .pickerStyle(.segmented)
-                    }
-                    
-                    Section(header: Text("Which multiplication table ?")) {
-                        Stepper(value: $multiplicationTable, in: 1...12) {
-                            Text("Multiplication table \(multiplicationTable)")
-                        }
-                        .animation(.default)
                     }
                 }
             }
             .navigationBarTitle("Edutainment")
         }
-        .onAppear {
-            Question()
+    }
+    
+    
+    
+    private func generateQuestion() {
+        questions = [Question]()
+        
+        for _ in 1...100 {
+            let random1 = Int.random(in: 1...multiplicationTable)
+            let random2 = Int.random(in: 1...12)
+            
+            let question = Question(text: "\(random1) * \(random2)", answer: random1 * random2)
+            questions.append(question)
+        }
+    }
+    
+    private func getNumberQuestion() -> Int {
+        switch questionSelection {
+        case 0:
+            return 5
+        case 1:
+            return 10
+        case 2:
+            return 20
+        default:
+            return questions.count
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
