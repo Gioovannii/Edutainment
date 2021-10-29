d612065711e7e86af71b93905d43a56c04a2a7a5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-29 14:56:01 +0800

    remove old code

--- Edutainment/Question.swift
+++ Edutainment/Question.swift
@@ -8,17 +8,6 @@
 import Foundation
 
 struct Question {
-    let multiplicationTable = 1
-    let answer = "0"
-    let questions = [String]()
-    
-    func getMultiplication(multiplicand: Int) -> Int {
-        var localQuestions = [String]()
-        for index in 1...12 {
-
-            print("\(multiplicationTable) X \(index) = \(index * multiplicationTable)")
-            localQuestions.append("\(index * multiplicationTable)")
-        }
-        return multiplicationTable * multiplicand
-    }
+    let text: String
+    let answer: Int
 }
